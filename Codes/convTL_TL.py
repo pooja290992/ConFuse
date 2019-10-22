461474d245ca7fb606bc1e8c25bd0ab4cde1d5c3
Pooja Gupta <poojag@iiitd.ac.in>
2019-10-22 22:40:57 +0530

    cleaned code

--- Codes/convTL_TL.py
+++ Codes/convTL_TL.py
@@ -448,9 +448,9 @@ features_list = ['CLOSE','OPEN','HIGH','LOW','CONTRACTS']
 # In[11]:
 
 
-out_pl1 = 4#20#4
-maxpl1 = False#True#False#True
-ks1 = 5#3#7#5
+out_pl1 = 4
+maxpl1 = False
+ks1 = 5
 mpl_ks = 2
 custom_batch_size_flag = False
 bs = 32
@@ -524,8 +524,7 @@ print('time taken for 125 stocks through all seeds : ',str(datetime.timedelta(se
 
 def ridge_regressor(Xtrain, Ytrain, Xtest, Ytest, alpha = 1.0, random_state = 1):
     clf = Ridge(alpha=alpha,random_state = random_state)
-    clf.fit(Xtrain, Ytrain) 
-    #print(clf.get_params())
+    clf.fit(Xtrain, Ytrain)
     y_pred = clf.predict(Xtest)
     #y_tr_pred = 
     mae = mean_absolute_error(Ytest, y_pred)
@@ -533,139 +532,6 @@ def ridge_regressor(Xtrain, Ytrain, Xtest, Ytest, alpha = 1.0, random_state = 1)
     rmse = math.sqrt(mse)
     return y_pred, mae, mse,rmse
 
-
-# In[14]:
-
-
-res_file_name = base_path+'Results2/Reg3/res_' + param_path + '_final.csv'
-pred_file_name = base_path+'Results2/Reg3/res_' + param_path + '_pred_global_final.csv'
-if os.path.exists(res_file_name):
-    os.remove(res_file_name)
-if os.path.exists(pred_file_name):
-    os.remove(pred_file_name)
-
-
-# In[15]:
-
-
-gap = 5.0
-log_interval = 1
-cnt = 0 
-# start = 0#1#0
-# end = 15#25#150
-alpha = 0.1#1.0#0.1
-random_state = 1
-test_acc_dict  = {}
-final_results_df = pd.DataFrame()
-t0 = time.time()
-for stock in stocks_list[start:end]:
-    t01 = time.time()
-    temp_dict = {}
-    test_acc_dict[stock] = {}
-    best_tr_acc = 0.0
-    best_te_acc = 0.0
-    best_ar = 0.0
-    best_mae = 100000.0
-    best_f1_score = 0.0
-    mae, mse, rmse = 0.0, 0.0, 0.0
-    found = 0
-    ytr_pred, yte_pred, tr_acc, te_acc, tr_scores, te_score = [],[],0,0,[],[]
-    AR, AnT, PoS, ApT, L, MpT, MlT, MaxCV, MinCV, IdleR = 0,0,0,0,0,0,0,0,0,0
-    temp_ytr_pred, temp_yte_pred, temp_tr_acc, temp_te_acc, temp_tr_scores, temp_te_scores = [],[],0,0,[],[]
-    temp_AR, temp_AnT, temp_PoS, temp_ApT, temp_L, temp_MpT, temp_MlT, temp_MaxCV, temp_MinCV, temp_IdleR = 0,0,0,0,0,0,0,0,0,0
-    prev_val_path = base_path + 'data/Reg3/TL_Test/' + stock + param_path +  '_' + str(test_size) +  '_tl_yprev_cp.npy'
-    prev_day_value = np.load(prev_val_path)
-    for sd in range(1,seed_range+1):
-        seed = sd
-        print('stock : ', stock)
-        xtr_path = base_path + 'data/Reg3/TL_Train/' + stock + param_path +'_' + str(test_size) + '_tl_xtrain' + str(seed) + '.npy'
-        ytr_path = base_path + 'data/Reg3/TL_Train/' + stock + param_path +  '_' + str(test_size) + '_tl_ytrain' + str(seed) + '.npy'
-        xte_path = base_path + 'data/Reg3/TL_Test/' + stock + param_path +  '_' + str(test_size) + '_tl_xtest' + str(seed) + '.npy'
-        yte_path = base_path + 'data/Reg3/TL_Test/' + stock + param_path +  '_' + str(test_size) + '_tl_ytest' + str(seed) + '.npy'
-        Ztrain = np.load(xtr_path)
-        Y_train = np.load(ytr_path)
-        Ztest = np.load(xte_path)
-        Y_test = np.load(yte_path)
-        print('seed : {: }'.format(seed))
-        yte_pred, mae, mse, rmse = ridge_regressor(Ztrain, Y_train, Ztest, Y_test, alpha = alpha, random_state = random_state)
-        pred_labels = np.where((yte_pred - prev_day_value)>0,1,0)
-        true_labels = np.where((Y_test - prev_day_value)>0,1,0)
-        te_acc = round(accuracy_score(true_labels, pred_labels)*100,3)
-        precision, recall, f1_score,_ = precision_recall_fscore_support(true_labels, pred_labels, pos_label=1, average='binary')
-        limit = Ztrain.shape[0]
-        #print(pred_labels)
-        mae2 = np.sum(np.abs(yte_pred-Y_test))/np.sum(Y_test)
-        print('mae2 :', mae2)
-        print('precision :{:.4f}, recall:{:.4f}, f1_score : {:.4f}'.format(precision, recall, f1_score))
-        if  f1_score > best_f1_score:
-            best_te_acc = te_acc
-            best_mae = mae
-            found = 1
-            best_f1_score = f1_score
-            test_acc_dict[stock]['seed'] = seed
-            test_acc_dict[stock]['mae'] = mae
-            test_acc_dict[stock]['mae2'] = mae2
-            test_acc_dict[stock]['mse'] = mse
-            test_acc_dict[stock]['rmse'] = rmse
-            test_acc_dict[stock]['Test Accuracy'] = te_acc
-            AR, AnT, PoS, ApT, L, MpT, MlT, MaxCV, MinCV, IdleR = compAnnualReturns(stock,pred_labels,data_df,window_size,limit)
-            test_acc_dict[stock]['AR'] = AR
-            test_acc_dict[stock]['Precision'] = precision
-            test_acc_dict[stock]['Recall'] = recall
-            test_acc_dict[stock]['F1_score'] = f1_score
-            temp_dict['pred_cp'] = yte_pred
-            temp_dict['Stock_Class'] = true_labels
-            temp_dict['ypred'] = pred_labels
-            temp_dict['found'] = found
-        
-    temp_final_df = pd.DataFrame(Y_test,columns=['actual_close_price'])
-    temp_final_df['predicted_close_price'] = temp_dict['pred_cp']
-    temp_final_df['prev_day_close_price'] = prev_day_value
-    temp_final_df['Stock_Class'] = temp_dict['Stock_Class']
-    temp_final_df['ypred'] = temp_dict['ypred']
-    temp_final_df['difference'] = temp_final_df['predicted_close_price'] - temp_final_df['actual_close_price']
-    temp_final_df['abs_difference'] = temp_final_df['difference'].abs()
-    temp_final_df['SYMBOL'] = stock
-    temp_final_df['found'] = found
-    final_results_df = pd.concat([final_results_df,temp_final_df],axis = 0)
-    cnt += 1
-    if cnt%log_interval==0:
-        df = pd.DataFrame.from_dict(data = test_acc_dict, orient = 'index').reset_index()
-        print('test_acc_dict data:')
-        print(df.head(2))
-        if not os.path.exists(res_file_name):
-            df.to_csv(res_file_name,index=None, header='column_names')
-        else: # else it exists so append without writing the header
-            df.to_csv(res_file_name, mode='a',index=None, header=False)
-        if not os.path.exists(pred_file_name):
-            final_results_df.to_csv(pred_file_name,index=None, header='column_names')
-        else: # else it exists so append without writing the header
-            final_results_df.to_csv(pred_file_name, mode='a',index=None, header=False)
-        print('final_results_df data:')
-        print(final_results_df.head(2))
-        test_acc_dict = {}
-        final_results_df = pd.DataFrame()
-    t11 = time.time()
-    print('time taken for one stock with ridge: ' ,datetime.timedelta(seconds = t11 - t01))
-    print('*'*100)
-t1 = time.time()
-print('time taken for all stocks with ridge: ' ,datetime.timedelta(seconds = t1 - t0))
-
-
-# # External Classifier
-
-# In[16]:
-
-
-rf_res_file_name = base_path+'Results2/Reg3/Classification/convTL_TL_classification_1layer_results4.csv'
-rf_pred_file_name = base_path+'Results2/Reg3/Classification/convTL_TL_classification_1layer_pred4.csv'
-if os.path.exists(rf_res_file_name):
-     os.remove(rf_res_file_name)
-if os.path.exists(rf_pred_file_name):
-     os.remove(rf_pred_file_name)
-
-
-# In[17]:
 
 
 def clfRF(Ztrain,Y_train,Ztest,Y_test,n_clf=5,depth=1,rnd_state=11):
@@ -681,133 +547,6 @@ def clfRF(Ztrain,Y_train,Ztest,Y_test,n_clf=5,depth=1,rnd_state=11):
     print("RF classification, test acc: {:2.2f}%".format(te_rf_score))
     return ytr_rf_pred, yte_rf_pred, tr_rf_score, te_rf_score, tr_scores, te_scores
 
-
-# In[18]:
-
-
-gap = 5.0
-avg_tr_acc = 0.0
-avg_te_acc = 0.0
-log_interval = 1
-tr_acc_min = 54
-cnt = 0 
-depth = 3
-num_clfs = 6
-pos_label = 1
-rf_test_acc_dict  = {}
-final_results_df = pd.DataFrame()
-t0 = time.time()
-for stock in stocks_list[start:end]:
-    t01 = time.time()
-    temp_dict = {}
-    rf_test_acc_dict[stock] = {}
-    best_tr_acc = 0.0
-    best_te_acc = 0.0
-    best_f1_score = 0.0
-    f1_score = 0.0
-    best_ar = 0.0
-    found = 0
-    ytr_pred, yte_pred, tr_acc, te_acc, tr_scores, te_score = [],[],0,0,[],[]
-    AR, AnT, PoS, ApT, L, MpT, MlT, MaxCV, MinCV, IdleR = 0,0,0,0,0,0,0,0,0,0
-    temp_ytr_pred, temp_yte_pred, temp_tr_acc, temp_te_acc, temp_tr_scores, temp_te_scores = [],[],0,0,[],[]
-    temp_f1_score = 0
-    temp_auc = 0
-    temp_AR, temp_AnT, temp_PoS, temp_ApT, temp_L, temp_MpT, temp_MlT, temp_MaxCV, temp_MinCV, temp_IdleR = 0,0,0,0,0,0,0,0,0,0
-    _,windowed_data,_, _ = getWindowedDataReg(data_df,stock,window_size)
-    feat_wise_data = getFeatWiseData(windowed_data,features_list)
-    prev_day_values = getPrevDayFeatures(feat_wise_data)
-    prev_day_values = prev_day_values[:,0]
-    for sd in range(1,seed_range+1):
-        seed = sd
-        print('stock : ', stock)
-        xtr_path = base_path + 'data/Reg3/TL_Train/' + stock + param_path +'_' + str(test_size) + '_tl_xtrain' + str(seed) + '.npy'
-        ytr_path = base_path + 'data/Reg3/TL_Train/' + stock + param_path +  '_' + str(test_size) + '_tl_ytrain' + str(seed) + '.npy'
-        xte_path = base_path + 'data/Reg3/TL_Test/' + stock + param_path +  '_' + str(test_size) + '_tl_xtest' + str(seed) + '.npy'
-        yte_path = base_path + 'data/Reg3/TL_Test/' + stock + param_path +  '_' + str(test_size) + '_tl_ytest' + str(seed) + '.npy'
-        Ztrain = np.load(xtr_path)
-        Y_train = np.load(ytr_path)
-        Ztest = np.load(xte_path)
-        Y_test = np.load(yte_path)
-        ytr_prev_day = prev_day_values[:Y_train.shape[0]]
-        yte_prev_day  = prev_day_values[Y_train.shape[0]:]
-        yte_prev_day  = yte_prev_day[:yte_prev_day.shape[0]-1]
-        for random_state in range(1,21):
-            print('seed : {: }, num_clfs : {: }, depth : {: }, random_state : {: }'.format(seed,num_clfs,depth,random_state))
-            Y_train_true_labels = np.where((Y_train - ytr_prev_day)>0,1,0)
-            Y_test_true_labels = np.where((Y_test - yte_prev_day)>0,1,0)
-            ytr_pred, yte_pred, tr_acc, te_acc, tr_scores, te_scores = clfRF(Ztrain,Y_train_true_labels, Ztest, Y_test_true_labels, n_clf=num_clfs,
-                                                                        depth=depth, rnd_state=random_state)
-            limit = Ztrain.shape[0]
-            AR, AnT, PoS, ApT, L, MpT, MlT, MaxCV, MinCV, IdleR = compAnnualReturns(stock,yte_pred,data_df,window_size,limit)
-            precision, recall, f1_score,_ = precision_recall_fscore_support(Y_test_true_labels, yte_pred, pos_label=1, average='binary')
-            fpr, tpr, thresholds = roc_curve(Y_test_true_labels, te_scores[:,pos_label], pos_label = pos_label)
-            AUC_val = auc(fpr, tpr)
-            print('AR: ',AR)
-            print('F1_score : ',f1_score)
-            if f1_score > best_f1_score:
-                #print('cmn........')
-                found = 1
-                best_ar = AR
-                best_te_acc = te_acc
-                best_f1_score = f1_score
-                rf_test_acc_dict[stock]['seed'] = seed
-                rf_test_acc_dict[stock]['depth'] = depth
-                rf_test_acc_dict[stock]['num_clfs'] = num_clfs
-                rf_test_acc_dict[stock]['random_state'] = random_state
-                rf_test_acc_dict[stock]['Tr Acc'] = tr_acc
-                rf_test_acc_dict[stock]['Te Acc'] = te_acc
-                rf_test_acc_dict[stock]['F1_score'] = f1_score
-                rf_test_acc_dict[stock]['Precision'] = round(precision,3)
-                rf_test_acc_dict[stock]['Recall'] = round(recall,3)
-                rf_test_acc_dict[stock]['AUC'] = round(AUC_val,3)
-                rf_test_acc_dict[stock]['AR'] = best_ar
-                rf_test_acc_dict[stock]['AnT'] = AnT
-                rf_test_acc_dict[stock]['PoS'] = PoS
-                rf_test_acc_dict[stock]['ApT'] = ApT
-                rf_test_acc_dict[stock]['L'] = L
-                rf_test_acc_dict[stock]['MpT'] = MpT 
-                rf_test_acc_dict[stock]['MlT'] = MlT
-                rf_test_acc_dict[stock]['MaxCV'] = MaxCV
-                rf_test_acc_dict[stock]['MinCV'] = MinCV
-                rf_test_acc_dict[stock]['IdleR'] = IdleR
-                rf_test_acc_dict[stock]['found'] = found
-                temp_dict['yte_pred'] = yte_pred
-                temp_dict['te_scores'] = te_scores
-    temp_final_df = pd.DataFrame(Y_test,columns=['ytrue'])
-    temp_final_df['ypred'] = temp_dict['yte_pred']
-    temp_scores_df = pd.DataFrame(temp_dict['te_scores']) 
-    temp_final_df = pd.concat([temp_final_df,temp_scores_df],axis = 1)
-    temp_final_df['SYMBOL'] = stock
-    final_results_df = pd.concat([final_results_df,temp_final_df],axis = 0)
-    cnt += 1
-    if cnt%log_interval==0:
-        df = pd.DataFrame.from_dict(data = rf_test_acc_dict, orient = 'index').reset_index()
-        print('rf_test_acc_dict data:')
-        print(df.head(2))
-        if not os.path.exists(rf_res_file_name):
-            df.to_csv(rf_res_file_name,index=None, header='column_names')
-        else: # else it exists so append without writing the header
-            df.to_csv(rf_res_file_name, mode='a',index=None, header=False)
-        if not os.path.exists(rf_pred_file_name):
-            final_results_df.to_csv(rf_pred_file_name,index=None, header='column_names')
-        else: # else it exists so append without writing the header
-            final_results_df.to_csv(rf_pred_file_name, mode='a',index=None, header=False)
-        print('final_results_df data:')
-        print(final_results_df.head(2))
-        rf_test_acc_dict = {}
-        final_results_df = pd.DataFrame()
-    avg_tr_acc += tr_acc
-    avg_te_acc += te_acc
-    t11 = time.time()
-    print('time taken for one stock grid-search tuning with RF: ' ,datetime.timedelta(seconds = t11 - t01))
-    print('*'*100)
-t1 = time.time()
-print('time taken for all stocks grid-search tuning with RF: ' ,datetime.timedelta(seconds = t1 - t0))
-avg_tr_acc = (avg_tr_acc)/cnt
-avg_te_acc = (avg_te_acc)/cnt
-
-
-# In[ ]:
 
 
 
